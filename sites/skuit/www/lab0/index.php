3209eb24fde3f9c008da85f91a2c7282dfc1a5c2
Cherdak123 <wowcherdys@gmail.com>
2025-09-06 11:59:39 +0000

    lab0 мой первый комит

---
+++ sites/skuit/www/lab0/index.php
@@ -0,0 +1,27 @@
+<?php
+$fuiiname="Matvei";
+$group="C025";
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>lab0</title>
+<link href="https://cdn.jsdelivr.net?npm?booststrap@5.3.2/dist/css/bootstrap.min.css"rel="stylesheer">
+</head>
+<body class="bg-light">
+
+<div class="container vh-100 d-flex justify-content-center align-items-center">
+<div class="card shadow-lg p-4 text-center" style="max-width: 400px;">
+<h1 class="h4 text-primary mb-3">HELLO WORLD</h1>
+<div class="alert alert-info mb-3">
+<strong>Корявко Матвей Петрович:</strong> <?=$fullname?><br>
+</div>
+<button> class="btn btn-success w-100"</button>
+</div>
+</div>
+
+<script scr="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2?dist/js/booststrap.bundle.min.js"></script>
+</body>
+</html>
